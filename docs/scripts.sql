f53449e901c353c1dcb6d8c9840c5a27aa9e280c
Evelyn Gomez <evelyngomez01@gmail.com>
2025-10-17 15:47:03 -0600

    Creación de scripts de subjects, groups y teachers

---
+++ docs/scripts.sql
@@ -0,0 +1,20 @@
+INSERT INTO subjects (name, semester, hours_week) VALUES
+('Física', 1, 4);
+('Fundamentos de Matemáticas', 1, 5),
+('Matemáticas Discretas', 1, 3),
+('Metodología de la Programación', 1, 5),
+('Programación Estructurada', 1, 5),
+('Taller de Competencias Informacionales', 1, 4),
+
+INSERT INTO "groups" (name, semestrer) VALUES 
+('M', 1);
+('N', 1);
+('O', 1);
+
+INSERT INTO teachers (name) VALUES 
+('Genaro Hernández Martínez');
+('Luis Alejandro Jonapa Chacon');
+('Alejandro Conde Escaroz');
+('Paola Lopez y Lopez');
+('Ricardo Vega Morales');
+('Ma. de los Angeles Polanco Enciso');
