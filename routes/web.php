999935b6d0e853be0323fd6665930dc60ba01f4f
AlondraGSD <alondra.snchez60@unach.mx>
2025-10-17 03:38:33 -0600

    Se creó la ruta de Groups

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\GroupController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('subjects', SubjectController::class);
+Route::resource('groups', GroupController::class);
