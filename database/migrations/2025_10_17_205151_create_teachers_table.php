a272e3b58a38db59e03164ed8b848765dfeab836
AlondraGSD <alondra.snchez60@unach.mx>
2025-10-19 13:50:43 -0600

    Se agregó el campo de categoría en la migración de Teachers

--- database/migrations/2025_10_17_205151_create_teachers_table.php
+++ database/migrations/2025_10_17_205151_create_teachers_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,9 +12,31 @@ return new class extends Migration
      */
     public function up(): void
     {
+        DB::statement("
+            DO $$
+            BEGIN
+                IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'teacher_category') THEN
+                    CREATE TYPE teacher_category AS ENUM (
+                        'Tiempo completo',
+                        'Medio tiempo',
+                        'Docentes de horas temporales sindicalizado',
+                        'Docentes de horas temporales no sindicalizado',
+                        'Docente de asignatura'
+                    );
+                END IF;
+            END$$;
+        ");
+
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
+            $table->enum('category', [ 
+                'Tiempo completo',
+                'Medio tiempo',
+                'Docentes de horas temporales sindicalizado',
+                'Docentes de horas temporales no sindicalizado',
+                'Docente de asignatura'
+            ])->default('Docente de asignatura');
             $table->timestamps();
         });
     }
@@ -24,5 +47,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('teachers');
+
+        DB::statement('DROP TYPE IF EXISTS teacher_category;');
     }
 };
