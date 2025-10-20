7c60ebab8680f40e8c79386c2c7cf0a9a15842f6
AlondraGSD <alondra.snchez60@unach.mx>
2025-10-20 16:51:09 -0600

    Se creó la ruta de Classrooms

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\GroupController;
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\ClassroomController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,4 @@ Route::get('/', function () {
 Route::resource('subjects', SubjectController::class);
 Route::resource('groups', GroupController::class);
 Route::resource('teachers', TeacherController::class);
+Route::resource('classrooms', ClassroomController::class);
