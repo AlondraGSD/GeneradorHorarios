a521060ada33e73bd2f30f155f44e34780617fa9
AlondraGSD <alondra.snchez60@unach.mx>
2025-10-22 02:01:12 -0600

    Se creó la ruta de Assigned_classes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\GroupController;
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\ClassroomController;
 use App\Http\Controllers\LaboratoryController;
+use App\Http\Controllers\AssignedClassController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,3 +17,4 @@ Route::resource('groups', GroupController::class);
 Route::resource('teachers', TeacherController::class);
 Route::resource('classrooms', ClassroomController::class);
 Route::resource('laboratories', LaboratoryController::class);
+Route::resource('assigned_classes', AssignedClassController::class);
